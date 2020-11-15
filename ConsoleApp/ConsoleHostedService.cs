576cbeddcacd463c840d2c62cc171a635c9be326
Richard Teller <rbrintont823@live.com>
2020-11-15 10:26:57 -0800

    Add project

---
+++ ConsoleApp/ConsoleHostedService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp
+{
+    public class ConsoleHostedService : IHostedService
+    {
+        private IWorker _worker { get; }
+
+        public ConsoleHostedService(IWorker worker)
+        {
+            _worker = worker ?? throw new ArgumentNullException(nameof(worker));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _worker.StartWork();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _worker.StopWork();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp
+{
+    // This console app was built from understanding: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-3.1
+    // and with some influence from: https://dfederm.com/building-a-console-app-with-.net-generic-host/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    // Add IHostedService
+                    services.AddHostedService<ConsoleHostedService>();
+
+                    // Add services
+                    services.AddSingleton<IWorker, Worker>();
+                });
+    }
+}
